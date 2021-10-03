c668854bd9bd1397771b71bf784512aa30a11c74
Pavithra <pavithragamage916@gmail.com>
2021-10-03 11:55:36 +0530

    single item page created

--- single_item_page.php
+++ single_item_page.php
@@ -5,7 +5,8 @@
     <div class="container">
         <h2 class="hero_headding">Processors > AMD Ryzen R9 3900X</h2>
         <hr class="hero_hr">
-        <p class="hero_para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+        <p class="hero_para">AMD Ryzen™ 9 3900X desktop processors from the AMD 3000 series are the most advanced desktop processors in the world for elite gamers. Built to Perform. Designed to Win.
+            The world's most advanced processor with 12 cores for the world's elite gamers.</p>
     </div>
 </div>
 <!--Hero Section End-->
@@ -14,11 +15,115 @@
 <div class="container">
     <div class="row item_row_main">
         <div class="row">
-            Carosal Div
+            <img src="images/ezgif.com-gif-maker.png" alt="" >
+
+
+            <div class="row">
+                <div class="col">
+
+                    <h3 class="item_price item_stock">In Stock</h3>
+                </div>
+                <div class="col">
+                    <h3 class="item_price">Price - Rs: 125,000</h3>
+                </div>
+                <div class="col cart_button">
+                    <button type="button" class="btn btn-secondary item_btn">Add to Cart <i class="fas fa-cart-arrow-down"></i></button>
+                </div>
+
+            </div>
+
+
 
         </div>
         <div class="row">
-            Table Div
+
+            <div class="item_table">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="item_table_col">Item Features</th>
+                            <th scope="col">Details</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Brand</th>
+                            <td>AMD</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Product Family</th>
+                            <td>AMD Ryzen™ Processors</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Product Line</th>
+                            <td>AMD Ryzen™ 9 Desktop Processors</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">CPU Cores</th>
+                            <td>12</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">CPU Threads</th>
+                            <td>24</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Max Boost Clock</th>
+                            <td>Up to 4.6GHz</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Base Clock</th>
+                            <td>3.8GHz</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Total L1 Cache</th>
+                            <td>768KB</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Total L2 Cache</th>
+                            <td>6MB</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Total L3 Cache</th>
+                            <td>64MB</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Default TDP</th>
+                            <td>105W</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">CMOS</th>
+                            <td>TSMC 7nm FinFET</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Unlock for Overclocking</th>
+                            <td>Yes</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">CPU Socket</th>
+                            <td>AM4</td>
+
+                        </tr>
+
+
+                    </tbody>
+                </table>
+            </div>
+
+
         </div>
     </div>
 
