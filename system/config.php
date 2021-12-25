c8e2e46cd5f7cfa8667f9a36f8be422f1e009b47
Pavithra <pavithragamage916@gmail.com>
2021-12-25 21:21:06 +0530

    back end development
    config the UI for backend

---
+++ system/config.php
@@ -0,0 +1,5 @@
+<?php 
+
+define("SITE_URL", "http://localhost/bit/system/" );
+
+?>
--- system/login.php
+++ system/login.php
@@ -11,7 +11,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PMS-Login</title>
+    <title>U-Star Digital System Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
